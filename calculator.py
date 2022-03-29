dfae92043884993e530c67b35655a3a85fc4597d
Nick <70932969+Nicksalve@users.noreply.github.com>
2022-03-29 17:42:49 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+first = input("enter your first number")
+operator = input("enter your operator (+,-,*,/,%)")
+second = input("enter your second number ")
+
+first = int(first)
+second = int(second)
+
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "/":
+    print(first / second)
+elif operator == "%":
+    print(first % second)
+else :
+    print("invalid o pration")
+    print("pleas check your numbers and the oprator")
